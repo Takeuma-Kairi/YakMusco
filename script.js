03472a5f550fe4fb3fbfd7cf49c8a7ef918685ca
Takeuma_Kairi <71685901+Takeuma-Kairi@users.noreply.github.com>
2023-07-16 22:16:36 +0900

    YakMusco1.2
    * 問題・キャラ番号の制御
    * 心不全、抗不整脈の追加

--- script.js
+++ script.js
@@ -24,7 +24,14 @@ var quiz= [[
 ["ソタロールは、心筋のKチャネル遮断作用とβ受容体遮断作用を示す", 0, "問題文の通り。ソタロールはⅢ群に分類される。", 0],
 ["ベラパミルはL型Caチャネルを遮断し、房室結節細胞の不応期を延長する", 0, "問題文の通り。ベラパミルはⅣ群に分類される。", 0],
 ["アトロピンはムスカリン性アセチルコリンM2受容体を遮断し、徐脈性不整脈を改善する", 0, "問題文の通り。アトロピンは洞性徐脈、房室ブロックを改善する。", 0],
-["アセブトロールはアドレナリンβ受容体を遮断し、異所性ペースメーカー活性を抑制する", 0, "問題文の通り。アセブトロールはⅡ群に分類される。",0]],
+["アセブトロールはアドレナリンβ受容体を遮断し、異所性ペースメーカー活性を抑制する", 0, "問題文の通り。アセブトロールはⅡ群に分類される。",0],
+["洞房結節において、心拍リズム形成に必要な興奮が発生する", 0, "問題文の通り。洞房結節は心拍リズム形成におけるペースメーカーである。", 0],
+["心電図において、QRS波は、心室の収縮のはじまりに対応している", 0, "問題文の通り。P波は心房の興奮、QRS波は心室の興奮、T波は心室の再分極を記録したものである。", 0],
+["心臓を支配する交感神経活動が亢進すると、心拍数が増加する", 0, "問題文の通り。交感神経が興奮すると、洞房結節の興奮頻度が増加し、刺激電動速度も増大するため、心拍数が増加する。",0],
+["心臓における興奮は、一連の特殊なニューロンにより伝導される。", 1, "心臓の刺激伝導系は特殊心筋と呼ばれる「心筋細胞」であり、神経細胞ではない。",0],
+["洞房結節は、一般にNaイオンの細胞内流入により活動電位を発生させ、刺激電動系におけるペースメーカーとして機能する", 1 , "Caイオンの細胞内流入により活動電位を発生させる。", 0],
+["心電図において、P波は心室の収縮のはじまりに対応している", 1, "心房の収縮を記録したものである。", 0]
+],
 
 [
 ["メチルジゴキシンは、Na,K-ATPaseを阻害して、心筋細胞内Ca濃度を上昇させて陽性変力作用を示す", 0, "問題文の通り。陽性変力作用とは、心筋収縮力増強作用のことである。", 0],
@@ -55,19 +62,24 @@ var chara=[[
 ["img/chara/proph.png", 0, "プロパフェノン","<p>Ⅰc群の抗不整脈薬。</p><p>Naイオンチャネルを遮断する。活動電位持続時間には影響を及ぼさない。</p><p>β受容体遮断作用をあわせもつため、徐脈や気管支痙攣のおそれがある。</p>"],
 ["img/chara/pils.png", 0, "ピルシカイニド","<p>Ⅰc群の抗不整脈薬。</p><p>リドカインに類似した構造を持つ。</p>"],
 ["img/chara/flec.png", 0, "フレカイニド","<p>Ⅰa群の抗不整脈薬。</p><p>プロカインアミドに類似した構造を持つ。</p>"],
-["img/chara/propra.png", 0, "プロプラノロール", "<p>Ⅱ群の抗不整脈薬。</p><p>β遮断薬。心拍数を下げることで頻脈性不整脈に効果を示す。</p><p>なお、これ以降、同様な構造（アミノプロパンジオール）をもったβ遮断薬には、「-olol」がステムとしてつく。</p>"],
-["img/chara/amio.png", 0, "アミオダロン", "<p>Ⅲ群の抗不整脈薬。</p>"],
-["img/chara/sota.png", 0, "ソタロール", "<p>Ⅲ群の抗不整脈薬。</p>"],
-["img/chara/nife.png", 0, "ニフェカラント", "<p>Ⅲ群の抗不整脈薬。</p>"],
+["img/chara/propra.png", 0, "プロプラノロール", "<p>Ⅱ群の抗不整脈薬。</p><p>β遮断薬。心拍数を下げることで頻脈性不整脈に効果を示す。</p><p>これ以外のβ遮断薬も、同じく頻脈性不整脈に用いられる。</p><p>なお、これ以降、同様な構造（アミノプロパンジオール）をもったβ遮断薬には、「-olol」がステムとしてつく。</p>"],
+["img/chara/amio.png", 0, "アミオダロン", "<p>Ⅲ群の抗不整脈薬。</p><p>Kイオンチャネルを遮断する。活動電位持続時間を延長する。</p><p>そのほか、β受容体、Kチャネル、Caチャネルなども遮断する。</p><p>肺線維症や間質性肺炎などの重篤な副作用がある。</p>"],
+["img/chara/sota.png", 0, "ソタロール", "<p>Ⅲ群の抗不整脈薬。</p><p>Kイオンチャネルを遮断する。活動電位持続時間を延長する。</p><p>そのほか、β受容体、Caチャネルなども遮断する。</p><p>むしろ、当初はβ遮断薬として使われていた。Kイオンチャネル遮断による抗不整脈作用は、後から発見された。薬名に「-olol」がついていないのは、プロプラノロールよりも前に発見されたものだから。</p>"],
+["img/chara/nife.png", 0, "ニフェカラント", "<p>Ⅲ群の抗不整脈薬。</p><p>Kイオンチャネルを遮断する。活動電位持続時間を延長する。</p>"],
 
-["img/chara/verap.png", 0, "ベラパミル", "<p>Ⅳ群の抗不整脈薬。</p>"],
-["img/chara/bepri.png", 0, "ベプリジル", "<p>Ⅳ群の抗不整脈薬。</p>"],
+["img/chara/verap.png", 0, "ベラパミル", "<p>Ⅳ群の抗不整脈薬。</p><p>洞房結節や房室結節のL型Caチャネルを遮断することで、頻脈性不整脈を改善する。</p><p>ケシ属の植物(poppy)から単離される、血管拡張・鎮痙剤「パパベリン」の誘導体。</p>"],
+["img/chara/bepri.png", 0, "ベプリジル", "<p>Ⅳ群の抗不整脈薬。</p><p>洞房結節や房室結節のL型Caチャネルを遮断することで、頻脈性不整脈を改善する。</p><p>その他、Naチャネル、Caチャネルの遮断作用を示す。</p>"],
 
-["img/chara/atro.png", 0, "アトロピン", "<p>ムスカリン性アセチルコリン受容体遮断薬。</p><p>ベラドンナという植物の根っこ（ベラドンナコン）由来。</p>"],
-["img/chara/isop.png", 0, "イソプレナリン", "<p>β刺激薬。</p><p>名前の由来は、「アドレナリン」のメチル基を「イソプロピル基」に変えた構造であることから。</p><p>Raymond P. Ahlquistは、本薬を用いた研究で、アドレナリン受容体がαとβという2種類に分けられることを発見した。"],
+["img/chara/atro.png", 0, "アトロピン", "<p>非選択的ムスカリン性アセチルコリン受容体遮断薬。</p><p>心拍数を増加させ、徐脈の治療に用いられる。</p><p>ベラドンナという植物の根っこ（ベラドンナコン）由来。</p><p>"],
+["img/chara/isop.png", 0, "イソプレナリン", "<p>β刺激薬。</p><p>β1受容体刺激による心機能を促進させる。</p><p>名前の由来は、「アドレナリン」のメチル基を「イソプロピル基」に変えた構造であることから。</p><p>Raymond P. Ahlquistは、本薬を用いた研究で、アドレナリン受容体がαとβという2種類に分けられることを発見した。</p>"]
 ],
 
-[["img/chara/digo.png", 1, "ジゴキシン", "<p>ジギタリス属の植物から抽出される強心配糖体。</p><p>Na/K-ATPaseを阻害することで間接的に細胞内Ca濃度を上昇させ、心筋の収縮力を増加させる。</p>"]]
+[["img/chara/digo.png", 1, "ジゴキシン・デスラノシド", "<p>ジギタリス属の植物から抽出される強心配糖体。</p><p>Na/K-ATPaseを阻害することで間接的に細胞内Ca濃度を上昇させ、心筋の収縮力を増加させる。</p>"],
+["img/chara/colfo.png", 0, "コルホルシンダロパート", "<p>アデニル酸シクラーゼを直接活性化する。cAMP濃度を上昇させることで、心機能を促進する。</p><p>長い薬名だが、スペル上はColforsin Daropateとなっており、「コルホルシン」に「ダロパート」がくっついたもの、という意味である。</p><p>コルホルシンは「フォルスコリン」という、コレウス・フォルスコリという植物から単離された物質のことを指し、例えばDHCのダイエットサプリ「フォースコリー」の成分はこれである。cAMP濃度上昇によるエネルギー代謝上昇による効果がある。</p><p>これにジメチルアミノプロパン酸（略してダロパート）をつけ、水溶性誘導体にしたものがコルホルシンダロパートである。</p>"],
+["img/chara/rinon.png", 0, "ミルリノン・オルプリノン", "<p>PDEⅢ阻害薬。</p><p>ミルリノン・オルプリノンは、いずれも「アムリノン」という薬剤の誘導体であるため、「リノン」が名についている。アムリノンよりも効果が増強され、副作用が低減している。</p>"],
+["img/chara/buda.png", 0, "ブクラデシン", "<p>cAMP誘導体。</p><p>ブチリル基をcAMPにくっつけたものなので、butyryl + cyclic + adenosine、略してbucladesine。</p>"],
+["img/chara/carpe.png", 0, "カルペリチド", "<p>心房性ナトリウム利尿ペプチド（ANP)製剤。</p><p>体内では心房から分泌され、腎臓に作用してNa排泄促進に伴う「利尿作用」と、血管平滑筋に作用する「血圧低下作用」をもつ。</p>"]
+]
 ];
 
 edu_unit = 0; //0:局所麻酔、1:不整脈
@@ -85,6 +97,7 @@ home_tachie = [0,0];
 function save_make(){
 	
 }
+
 //======================================================
 function bookmarking(){
 	var img_bookmark = document.getElementById("bookmark");
@@ -127,6 +140,17 @@ function change_edu_unit(n){
 }
 
 //======================================================
+function change_mondai(n){
+  quiz_num = parseInt(n)-1;
+  quiz_init();
+}
+
+//======================================================
+function change_chara(n){
+  change_img(parseInt(n)-1);
+}
+
+//======================================================
 /*function change_home_chara(){
 	var next_chara_num = home_tachie[edu_unit] + 1;
 	
@@ -146,6 +170,46 @@ function change_edu_unit(n){
 	home_tachie[edu_unit] = next_chara_num;
 	div_serifu.innerHTML =chara[edu_unit][next_chara_num][2];
 } */
+
+function mondai_select_add(){
+  mondai_select = document.getElementById("mondai_select");
+  select_length = mondai_select.options.length;
+  if(mondai_select.options.length != quiz[edu_unit].length){
+    
+    for(var i=0; i<select_length; i++){
+      mondai_select.options.remove(0);
+    }
+  
+    for(var j=0; j<quiz[edu_unit].length; j++){
+      var temp_option = document.createElement("option");
+      temp_option.value = j+1;
+      temp_option.text = j+1;
+      mondai_select.add(temp_option, null);
+    }
+  }
+  mondai_select.options[quiz_num].selected = true;
+}
+
+//======================================================
+function chara_select_add(){
+  chara_select = document.getElementById("chara_select");
+  select_length = chara_select.options.length;
+  if(chara_select.options.length != chara[edu_unit].length){
+    
+    for(var i=0; i<select_length; i++){
+      chara_select.options.remove(0);
+    }
+    
+    for(var j=0; j<chara[edu_unit].length; j++){
+      var temp_option = document.createElement("option");
+      temp_option.value = j+1;
+      temp_option.text = j+1;
+      chara_select.add(temp_option, null);
+    }
+  }
+  
+  chara_select.options[chara_num].selected = true;
+}
 //======================================================
 function change_img(n){
 var src_temp = "img/chara/chara0.png";
@@ -179,7 +243,8 @@ right_btn.src = "img/right.png";
 	}else if(n >= chara[edu_unit].length - 1){
 		right_btn.src = "img/right_not_active.png";
 	}
-		
+  
+  chara_select_add();
 }
 //======================================================
 function answering(n){
@@ -254,6 +319,8 @@ function quiz_show(){
   var maru = document.getElementById("maru");
   var batu = document.getElementById("batu");
   var next_question = document.getElementById("next_question");
+  var mondai_select_label = document.getElementById("mondai_select_label");
+  var mondai_select = document.getElementById("mondai_select");
   
   img_bookmark.style.display = "inline";
   maru.src = "img/maru.png";
@@ -263,18 +330,25 @@ function quiz_show(){
   batu.style.display = "inline";
   next_question.style.display = "inline";
   
-  var q1 = document.getElementById("q1"); 
-  var q2 = document.getElementById("q2"); 
-  var q3 = document.getElementById("q3"); 
-  var result = document.getElementById("result"); 
+  mondai_select.style.display = "inline-block";
+  mondai_select_label.style.display = "inline-block";
   
-  q1.src = "img/q_not_active.png";
+  
+  mondai_select_add();
+  
+  /* var q1 = document.getElementById("q1"); 
+  var q2 = document.getElementById("q2"); 
+  var q3 = document.getElementById("q3");  */
+  /* var result = document.getElementById("result");  */
+  
+  /* q1.src = "img/q_not_active.png";
   q2.src = "img/q_not_active.png";
   q3.src = "img/q_not_active.png";
-  result.src = "img/result_not_active.png";
+  result.src = "img/result_not_active.png"; */
   
 	//---
-  var mondaibun = '<p><span class="caption_span">第' + (quiz_monme_num + 1) + '問 (問題番号：' + (quiz_num + 1) + ")</span></p>" + quiz[edu_unit][quiz_num][0];
+  var mondaibun = '<p><span class="caption_span">第' + (quiz_monme_num + 1) + '問</span></p>' + quiz[edu_unit][quiz_num][0]; ;
+  /* (問題番号：' + (quiz_num + 1) + ")</span></p>" + quiz[edu_unit][quiz_num][0]; */
   mondai.innerHTML = mondaibun;
 	
 	//---
@@ -286,17 +360,18 @@ function quiz_show(){
 	}else{
 		img_bookmark.src="img/bookmarked.png";
 	}
-	
-  switch(quiz_monme_num){
-    case 3:
-      result.src = "img/result_normal.png"
-			
+
+  /* switch(quiz_monme_num){
+    case 3: */
+      /* result.src = "img/result_normal.png" */
+		if(quiz_monme_num == 3){	
       img_bookmark.style.display = "none";
       maru.style.display = "none";
       batu.style.display = "none";
       next_question.style.display = "none";
 			
-			
+      mondai_select_label.style.display = "none";
+      mondai_select.style.display = "none";
 			
       mondaibun = "<h1>結果は…</h1><p>" + seikaisu + "/3問正解</p>" + 
 									'<p><button onclick="quiz_restart()">もう一回やる</button><button onclick="quiz_init()">次の問題セットに進む</button></p>';
@@ -316,19 +391,20 @@ function quiz_show(){
 			      
      mondai.innerHTML = mondaibun;
      mondai.className="mondai mondai_result";
-		 
-    case 2:
+		}
+    
+    /* case 2:
       q3.src = "img/q_normal.png";
     case 1:
       q2.src = "img/q_normal.png";
     case 0:
       q1.src = "img/q_normal.png";
-      break;
-		
+      break; */
+		/* 
 		default:
 			alert("エラー。現在の回答フェーズ：" + quiz_monme_num);
 			break;
-  }
+  } */
   
   
 }
